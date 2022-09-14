1923f91affb3c85314d668b28263a6ded8b5768e
arcticfloyd1984 <yashasvichaudhry98@gmail.com>
2022-09-14 14:46:27 +0400

    more files added to previous commit

--- relayer/src/services/transaction-publisher/index.ts
+++ relayer/src/services/transaction-publisher/index.ts
@@ -1,3 +1,3 @@
-export * from './transaction-publisher';
-export * from './interface/interface';
+export * from './TransactionPublisher';
+export * from './interface/ITransactionPublisher';
 export * from './types/types';
