f34a840f98774e3d0b14b45b95cd19f44123ea47
arcticfloyd1984 <yashasvichaudhry98@gmail.com>
2022-09-27 12:29:00 +0400

    TransactionPublisher service removed + restructured TransactionListener

--- relayer/src/services/transaction-listener/interface/ITransactionListener.ts
+++ relayer/src/services/transaction-listener/interface/ITransactionListener.ts
@@ -1,14 +1,12 @@
 import { ITransactionDAO } from '../../../../../common/db';
-import { IQueue } from '../../../../../common/interface';
 import { INetworkService } from '../../../../../common/network';
 import { EVMRawTransactionType } from '../../../../../common/types';
 import { IEVMAccount } from '../../account/interface/IEVMAccount';
-import { NotifyTransactionListenerParamsType, TransactionListenerMessageType } from '../types';
+import { NotifyTransactionListenerParamsType } from '../types';
 
 export interface ITransactionListener {
   chainId: number;
   networkService: INetworkService<IEVMAccount<EVMRawTransactionType>, EVMRawTransactionType>;
-  queue: IQueue<TransactionListenerMessageType>;
   transactionDao: ITransactionDAO;
 
   notify(notifyTransactionListenerParams: NotifyTransactionListenerParamsType): Promise<void>
