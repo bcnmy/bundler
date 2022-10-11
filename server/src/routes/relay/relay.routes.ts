5fd1fd051088807a9e2e0d4752000ebc1733d04e
arcticfloyd1984 <yashasvichaudhry98@gmail.com>
2022-10-11 15:16:44 +0400

    Commented out simulation

--- server/src/routes/relay/relay.routes.ts
+++ server/src/routes/relay/relay.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { feeOptionsApi, requestHandler } from '../../controllers';
-import { simulateTransaction } from '../../controllers/simulate';
+// import { simulateTransaction } from '../../controllers/simulate';
 import { validateRelayRequest, validateRequest } from '../../middleware';
 import { feeOptionsSchema } from './relay.schema';
 
 export const relayApiRouter = Router();
 
 relayApiRouter.get('/feeOptions', validateRequest(feeOptionsSchema), feeOptionsApi);
-relayApiRouter.post('/', validateRelayRequest(), simulateTransaction, requestHandler);
+relayApiRouter.post('/', validateRelayRequest(), requestHandler);
