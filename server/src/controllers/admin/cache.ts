2646084f37ffd81869ff7f09c8ee0806f3ee3953
Kunal Chauhan <chauhan.kunal4@gmail.com>
2022-11-16 00:32:23 +0530

    added admin apis to update cache

--- server/src/controllers/admin/cache.ts
+++ server/src/controllers/admin/cache.ts
@@ -24,3 +24,59 @@ export const deleteCacheAPI = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getCacheAPI = async (req: Request, res: Response) => {
+  try {
+    const { secret } = req.query;
+    const { key } = req.body;
+    if (secret !== 'j2AFV5Ni89kcU6jM') {
+      return res.status(400).json({
+        error: 'invalid request',
+      });
+    }
+    const data = await RedisCacheService.getInstance().get(key);
+    if (data) {
+      return res.json({
+        msg: 'fetched cache',
+        data,
+      });
+    }
+
+    return res.status(404).json({
+      msg: 'cache not found',
+    });
+  } catch (error) {
+    log.error(`Error in deleteCacheAPI ${error}`);
+    return res.status(500).json({
+      error,
+    });
+  }
+};
+
+export const postCacheAPI = async (req: Request, res: Response) => {
+  try {
+    const { secret } = req.query;
+    const { key, value } = req.body;
+    if (secret !== 'j2AFV5Ni89kcU6jM') {
+      return res.status(400).json({
+        error: 'invalid request',
+      });
+    }
+    const data = await RedisCacheService.getInstance().set(key, value);
+    if (data) {
+      return res.json({
+        msg: 'saved in cache',
+        data: value,
+      });
+    }
+
+    return res.status(400).json({
+      msg: 'cache not saved',
+    });
+  } catch (error) {
+    log.error(`Error in deleteCacheAPI ${error}`);
+    return res.status(500).json({
+      error,
+    });
+  }
+};
--- server/src/routes/admin/admin.routes.ts
+++ server/src/routes/admin/admin.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { settings, status, deleteCacheAPI } from '../../controllers';
+import {
+  settings, status, deleteCacheAPI, getCacheAPI, postCacheAPI,
+} from '../../controllers';
 
 export const adminApiRouter = Router();
 
 adminApiRouter.get('/', settings);
 adminApiRouter.get('/status', status);
-adminApiRouter.post('/cache/delete', deleteCacheAPI);
+
+adminApiRouter.post('/cache', postCacheAPI);
+adminApiRouter.get('/cache', getCacheAPI);
+adminApiRouter.delete('/cache', deleteCacheAPI);
