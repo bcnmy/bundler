cb8acad6b9c040eee35107d11790a21bb78b8713
arcticfloyd1984 <yashasvichaudhry98@gmail.com>
2023-01-03 17:50:51 +0530

    Added 30K of extra gas in gas limit for SCW txns

--- common/simulation/external-simulation/TenderlySimulationService.ts
+++ common/simulation/external-simulation/TenderlySimulationService.ts
@@ -97,7 +97,7 @@ export class TenderlySimulationService implements IExternalSimulation {
     return {
       isSimulationSuccessful: true,
       msgFromSimulation: 'Fee options fetched successfully',
-      gasLimitFromSimulation: gasLimitFromSimulation + 300000,
+      gasLimitFromSimulation: gasLimitFromSimulation + 30000,
     };
   }
 
