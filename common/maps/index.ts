c899ed2b0e1a8672050c5aa46c1138c10dfdc800
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2023-03-28 23:50:56 +0530

    Bundler shifted to RM1

--- common/maps/index.ts
+++ common/maps/index.ts
@@ -7,5 +7,5 @@ export const relayerManagerTransactionTypeNameMap = {
   [TransactionType.GASLESS_FALLBACK]: 'RM1',
   [TransactionType.CROSS_CHAIN]: 'RM2',
   [TransactionType.FUNDING]: 'RM0',
-  [TransactionType.BUNDLER]: 'RM3',
+  [TransactionType.BUNDLER]: 'RM1',
 };
