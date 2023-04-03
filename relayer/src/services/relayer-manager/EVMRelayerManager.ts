0ed93f12e3d19bd8d729f22a5249a1963df8dbb9
Kunal Chauhan <chauhan.kunal4@gmail.com>
2023-04-03 11:12:02 +0530

    fix: add active relayer back to the list of relayers

--- relayer/src/services/relayer-manager/EVMRelayerManager.ts
+++ relayer/src/services/relayer-manager/EVMRelayerManager.ts
@@ -230,9 +230,11 @@ implements IRelayerManager<IEVMAccount, EVMRawTransactionType> {
     if (relayer) {
       // check if pending count of relayer is less than threshold
       // else you wait for the transaction to be mined
-      if (relayer.pendingCount < this.pendingTransactionCountThreshold) {
-        await this.relayerQueue.push(relayer);
-      }
+      await this.relayerQueue.push(relayer);
+      // TODO: uncomment below code once we have a way to check if transaction is mined
+      // if (relayer.pendingCount < this.pendingTransactionCountThreshold) {
+      //   await this.relayerQueue.push(relayer);
+      // }
       delete this.transactionProcessingRelayerMap[address];
 
       // check if size of active relayer queue is
