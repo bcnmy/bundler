20f3e026e0b6dc760251bbdd68ad437f78db9358
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2023-04-03 11:29:33 +0530

    logs in addActiveRelayer

--- relayer/src/services/relayer-manager/EVMRelayerManager.ts
+++ relayer/src/services/relayer-manager/EVMRelayerManager.ts
@@ -230,6 +230,7 @@ implements IRelayerManager<IEVMAccount, EVMRawTransactionType> {
     if (relayer) {
       // check if pending count of relayer is less than threshold
       // else you wait for the transaction to be mined
+      log.info(`For relayer: ${relayer.address} pendingCount: ${relayer.pendingCount} pendingTransactionCountThreshold: ${this.pendingTransactionCountThreshold} of Relayer Manager: ${this.name} on chainId: ${this.chainId}`);
       await this.relayerQueue.push(relayer);
       // TODO: uncomment below code once we have a way to check if transaction is mined
       // if (relayer.pendingCount < this.pendingTransactionCountThreshold) {
