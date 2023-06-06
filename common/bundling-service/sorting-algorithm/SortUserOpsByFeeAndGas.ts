536afe0f0dac35ee70680d0561076c03f771687f
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2023-06-06 12:40:46 +0530

    userOp sorting logic now just uses maxPriorityFee

--- common/bundling-service/sorting-algorithm/SortUserOpsByFeeAndGas.ts
+++ common/bundling-service/sorting-algorithm/SortUserOpsByFeeAndGas.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import { BigNumber } from 'ethers';
 import { EVMRawTransactionType, UserOperationType } from '../../types';
 import { INetworkService } from '../../network';
@@ -15,9 +16,9 @@ export class SortUserOpsByFeeAndGas {
   }
 
   async sort(userOps: UserOperationType[]): Promise<UserOperationType[]> {
-    userOps.sort(async (userOp1: UserOperationType, userOp2: UserOperationType) => {
-      const userOp1GasPrice = await this.getUserOpGasPrice(userOp1);
-      const userOp2GasPrice = await this.getUserOpGasPrice(userOp2);
+    userOps.sort((userOp1: UserOperationType, userOp2: UserOperationType) => {
+      const userOp1GasPrice = this.getUserOpGasPrice(userOp1);
+      const userOp2GasPrice = this.getUserOpGasPrice(userOp2);
       if (userOp1GasPrice === userOp2GasPrice) {
         return BigNumber.from(
           userOp1.preVerificationGas,
@@ -28,17 +29,12 @@ export class SortUserOpsByFeeAndGas {
     return userOps;
   }
 
-  private async getUserOpGasPrice(userOp: UserOperationType): Promise<number> {
+  private getUserOpGasPrice(userOp: UserOperationType): number {
     const { maxFeePerGas, maxPriorityFeePerGas } = userOp;
     if (maxFeePerGas === maxPriorityFeePerGas) {
       // legacy mode (for networks that don't support basefee opcode)
       return BigNumber.from(maxFeePerGas).toNumber();
     }
-    // TODO make it fetch from cache
-    const baseFeeForBlock = await this.networkService.getBaseFeeForBlock();
-    return Math.min(
-      BigNumber.from(maxFeePerGas).toNumber(),
-      BigNumber.from(maxPriorityFeePerGas).toNumber() + BigNumber.from(baseFeeForBlock).toNumber(),
-    );
+    return BigNumber.from(maxPriorityFeePerGas).toNumber();
   }
 }
