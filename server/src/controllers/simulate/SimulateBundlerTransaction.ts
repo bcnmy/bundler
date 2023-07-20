87a21656071e9bbd6fd317326a674d0df6ec2d5c
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2023-07-20 12:29:38 +0200

    added restirction on testnet rpc

--- server/src/controllers/simulate/SimulateBundlerTransaction.ts
+++ server/src/controllers/simulate/SimulateBundlerTransaction.ts
@@ -11,8 +11,18 @@ export const validateBundlerTransaction = async (req: Request) => {
   try {
     const userOp = req.body.params[0];
     const entryPointAddress = req.body.params[1];
-    const { chainId } = req.params;
+    const { chainId, dappAPIKey } = req.params;
     log.info(`chainId from request params: ${chainId}`);
+    log.info(`dappAPIKey from request params: ${dappAPIKey}`);
+
+    if (dappAPIKey === 'nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44') {
+      if (![5, 80001, 97, 1442, 421613, 420, 43113, 84531, 59140].includes(parseInt(chainId, 10))) {
+        return {
+          code: -32400,
+          message: 'Api Key does not match with registered chainId',
+        };
+      }
+    }
 
     const entryPointContracts = entryPointMap[parseInt(chainId, 10)];
 
