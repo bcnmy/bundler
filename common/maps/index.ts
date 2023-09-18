d2d3a915ddf4511ea06c8381aefed333f5917873
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2023-09-18 12:51:45 +0530

    synced with staging

--- common/maps/index.ts
+++ common/maps/index.ts
@@ -3,7 +3,7 @@ import { TransactionType } from '../types';
 // change below to assign relayer manager to transaction type
 export const relayerManagerTransactionTypeNameMap = {
   [TransactionType.AA]: 'RM1',
-  [TransactionType.SCW]: 'RM2',
+  [TransactionType.SCW]: 'RM1',
   [TransactionType.FUNDING]: 'RM0',
   [TransactionType.BUNDLER]: 'RM1',
 };
