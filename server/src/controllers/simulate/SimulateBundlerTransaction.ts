d3818aa8ec360a338691cdfe955dc28934982db3
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2023-11-21 09:51:51 +0530

    Added Linea testnet

--- server/src/controllers/simulate/SimulateBundlerTransaction.ts
+++ server/src/controllers/simulate/SimulateBundlerTransaction.ts
@@ -19,7 +19,7 @@ export const validateBundlerTransaction = async (req: Request) => {
 
     if (dappAPIKey === 'nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44') {
       if (![5, 80001, 97, 1442, 421613, 420,
-        43113, 84531, 59140, 5001, 5611, 81, 88882, 59140].includes(parseInt(chainId, 10))) {
+        43113, 84531, 59140, 5001, 5611, 81, 88882, 59144].includes(parseInt(chainId, 10))) {
         return {
           code: -32400,
           message: 'Api Key does not match with registered chainId',
