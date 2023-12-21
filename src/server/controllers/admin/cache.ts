e0d79599d6dd1354da48d01aac02b6ce02b3ab00
Nikola Divic <divicnikola@gmail.com>
2023-12-21 17:06:14 +0100

    refactor: :art: reorganize the directory layout
    All of the TS source code is now in the src folder and the transpiled JS is in dist. Also I deleted the redundant nested directories
    
    This is a big code reorganization, be careful

---
+++ src/server/controllers/admin/cache.ts
@@ -0,0 +1,86 @@
+/* eslint-disable import/no-import-module-exports */
+import { Request, Response } from "express";
+import { RedisCacheService } from "../../../common/cache";
+import { logger } from "../../../common/logger";
+import { STATUSES } from "../../middleware";
+
+const log = logger.child({
+  module: module.filename.split("/").slice(-4).join("/"),
+});
+
+export const deleteCacheAPI = async (req: Request, res: Response) => {
+  try {
+    const { secret } = req.query;
+    const { key } = req.body;
+    if (secret !== "j2AFV5Ni89kcU6jM") {
+      return res.status(STATUSES.BAD_REQUEST).json({
+        error: "invalid request",
+      });
+    }
+    await RedisCacheService.getInstance().delete(key);
+    return res.json({
+      msg: "deleted cache",
+    });
+  } catch (error) {
+    log.error(`Error in deleteCacheAPI ${error}`);
+    return res.status(STATUSES.INTERNAL_SERVER_ERROR).json({
+      error,
+    });
+  }
+};
+
+export const getCacheAPI = async (req: Request, res: Response) => {
+  try {
+    const { secret } = req.query;
+    const { key } = req.body;
+    if (secret !== "j2AFV5Ni89kcU6jM") {
+      return res.status(STATUSES.BAD_REQUEST).json({
+        error: "invalid request",
+      });
+    }
+    const data = await RedisCacheService.getInstance().get(key);
+    if (data) {
+      return res.json({
+        msg: "fetched cache",
+        data,
+      });
+    }
+
+    return res.status(STATUSES.NOT_FOUND).json({
+      msg: "cache not found",
+    });
+  } catch (error) {
+    log.error(`Error in deleteCacheAPI ${error}`);
+    return res.status(STATUSES.INTERNAL_SERVER_ERROR).json({
+      error,
+    });
+  }
+};
+
+export const postCacheAPI = async (req: Request, res: Response) => {
+  try {
+    const { secret } = req.query;
+    const { key, value } = req.body;
+    if (secret !== "j2AFV5Ni89kcU6jM") {
+      return res.status(STATUSES.BAD_REQUEST).json({
+        error: "invalid request",
+      });
+    }
+    const data = await RedisCacheService.getInstance().set(key, value);
+    if (data) {
+      return res.json({
+        msg: "saved in cache",
+        data: value,
+      });
+    }
+
+    return res.status(STATUSES.BAD_REQUEST).json({
+      msg: "cache not saved",
+    });
+  } catch (error) {
+    log.error(`Error in deleteCacheAPI ${error}`);
+    return res.status(STATUSES.INTERNAL_SERVER_ERROR).json({
+      error,
+    });
+  }
+};
