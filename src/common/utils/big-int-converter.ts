1b15fde5c465a7508728e677f0adba98a186ee53
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2024-01-03 17:46:49 +0530

    using bigIntToNumber instead of bigIntToString

--- src/common/utils/big-int-converter.ts
+++ src/common/utils/big-int-converter.ts
@@ -1,3 +1,31 @@
+export const bigIntToNumber = (args: any): any => {
+  if(typeof args === 'object') {
+    const result: any = {};
+
+    for (const [key, value] of Object.entries(args)) {
+      if (typeof value === 'bigint') {
+        // Convert BigInt to string
+        result[key] = value.toString();
+      } else {
+        result[key] = value;
+      }
+    }
+    return result;
+  } 
+    const result: any = [];
+
+    for (const element of args) {
+      if (typeof element === 'bigint') {
+        // Convert BigInt to string
+        result.push(Number(element));
+      } else {
+        result.push(element);
+      }
+    }
+    return result;
+};
+
+
 export const bigIntToString = (object: any): any => {
   const result: any = {};
 
@@ -5,6 +33,8 @@ export const bigIntToString = (object: any): any => {
     if (typeof value === 'bigint') {
       // Convert BigInt to string
       result[key] = value.toString();;
+    } else if ((value as any).length >= 0) {
+      result[key] = bigIntToNumber(value);
     } else {
       result[key] = value;
     }
