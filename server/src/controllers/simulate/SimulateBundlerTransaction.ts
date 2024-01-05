a983db4242d0bb730b183a0c4bba9deee995a3ed
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2024-01-05 11:47:24 +0530

    added all testnet networks and improved msg

--- server/src/controllers/simulate/SimulateBundlerTransaction.ts
+++ server/src/controllers/simulate/SimulateBundlerTransaction.ts
@@ -18,11 +18,15 @@ export const validateBundlerTransaction = async (req: Request) => {
     log.info(`dappAPIKey from request params: ${dappAPIKey}`);
 
     if (dappAPIKey === 'nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44') {
-      if (![5, 80001, 97, 1442, 421613, 420,
-        43113, 84531, 59140, 5001, 5611, 81, 88882, 59144].includes(parseInt(chainId, 10))) {
+      if (
+        ![
+          5, 80001, 97, 1442, 421613, 420, 43113, 84531, 59140, 5001, 5611, 81,
+          88882, 59144, 421614, 11155111, 7116, 1115, 3441005,
+        ].includes(parseInt(chainId, 10))
+      ) {
         return {
           code: -32400,
-          message: 'Api Key does not match with registered chainId',
+          message: 'Request to mainnet not allowed. Please reach out and request a mainnet Bundler URL',
         };
       }
     }
