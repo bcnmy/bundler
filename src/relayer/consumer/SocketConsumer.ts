e672dc0b38f162e0de1f6cb004763df60536074a
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2024-01-23 21:07:32 +0530

    error handling around creation of CentClient

--- src/relayer/consumer/SocketConsumer.ts
+++ src/relayer/consumer/SocketConsumer.ts
@@ -21,7 +21,7 @@ const log = logger.child({
 export class SocketConsumer implements ISocketConsumer {
   chainId: number;
 
-  socketClient: CentClient;
+  socketClient: CentClient | null = null;
 
   private queue: IQueue<TransactionQueueMessageType>;
 
@@ -33,10 +33,14 @@ export class SocketConsumer implements ISocketConsumer {
 
   constructor(socketConsumerParams: SocketConsumerParamsType) {
     const { options, queue } = socketConsumerParams;
-    this.socketClient = new CentClient({
-      url: config.socketService.httpUrl,
-      token: config.socketService.apiKey,
-    });
+    try {
+      this.socketClient = new CentClient({
+        url: config.socketService.httpUrl,
+        token: config.socketService.apiKey,
+      });
+    } catch (error) {
+      log.error(`Error in setting up cent client`);
+    }
     this.chainId = options.chainId;
     this.queue = queue;
     this.EVMRelayerManagerMap = options.EVMRelayerManagerMap;
@@ -66,6 +70,9 @@ export class SocketConsumer implements ISocketConsumer {
           ][this.chainId].postTransactionMined(
             transactionDataReceivedFromQueue.receipt?.from,
           );
+        }
+        if (this.socketClient === null) {
+          throw new Error(`socketClient instance is null`);
         }
         this.socketClient.publish({
           channel: `transaction:${transactionDataReceivedFromQueue.transactionId}`,
