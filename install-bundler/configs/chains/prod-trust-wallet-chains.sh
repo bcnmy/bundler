c9ac6c17fb5457467d89f56025132af4ee40e93a
Takwa <takwa.gnenna@outlook.com>
2024-04-03 13:05:51 +0100

    deploy only mumbai on prod

--- install-bundler/configs/chains/prod-trust-wallet-chains.sh
+++ install-bundler/configs/chains/prod-trust-wallet-chains.sh
@@ -8,66 +8,66 @@ declare -A chain_mumbai=(
   [maxReplica]=20
   )
 
-declare -A chain_bnb=(
-  [name]='chain-56'
-  [chainId]="56"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=6
-  [maxReplica]=6
-)
+# declare -A chain_bnb=(
+#   [name]='chain-56'
+#   [chainId]="56"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=6
+#   [maxReplica]=6
+# )
 
-declare -A chain_polygon=(
-  [name]='chain-137'
-  [chainId]="137"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=6
-  [maxReplica]=6)
-
-
-declare -A chain_arbitrum=(
-  [name]='chain-42161'
-  [chainId]="42161"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=3
-  [maxReplica]=3
-)
+# declare -A chain_polygon=(
+#   [name]='chain-137'
+#   [chainId]="137"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=6
+#   [maxReplica]=6)
 
 
-declare -A chain_avalanche=(
-  [name]='chain-43114'
-  [chainId]="43114"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=3
-  [maxReplica]=3
-)
+# declare -A chain_arbitrum=(
+#   [name]='chain-42161'
+#   [chainId]="42161"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=3
+#   [maxReplica]=3
+# )
 
-declare -A chain_opBNB=(
-  [name]='chain-204'
-  [chainId]="204"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=900m
-  [minReplica]=3
-  [maxReplica]=3
-)
 
-declare -A chain_base=(
-  [name]='chain-8453'
-  [chainId]="8453"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=3
-  [maxReplica]=3
-)
+# declare -A chain_avalanche=(
+#   [name]='chain-43114'
+#   [chainId]="43114"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=3
+#   [maxReplica]=3
+# )
 
-declare -A chain_optimism=(
-  [name]='chain-10'
-  [chainId]="10"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=3
-  [maxReplica]=3
-)
+# declare -A chain_opBNB=(
+#   [name]='chain-204'
+#   [chainId]="204"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=900m
+#   [minReplica]=3
+#   [maxReplica]=3
+# )
+
+# declare -A chain_base=(
+#   [name]='chain-8453'
+#   [chainId]="8453"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=3
+#   [maxReplica]=3
+# )
+
+# declare -A chain_optimism=(
+#   [name]='chain-10'
+#   [chainId]="10"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=3
+#   [maxReplica]=3
+# )
