c2effd81831d123ec5692cbd1c5a395ad125a74e
Takwa <takwa.gnenna@outlook.com>
2024-04-03 13:03:40 +0100

    deploy only mumbain on prod

--- install-bundler/configs/chains/staging-trust-wallet-chains.sh
+++ install-bundler/configs/chains/staging-trust-wallet-chains.sh
@@ -1,9 +1,9 @@
 
-declare -A chain_mumbai=(
-  [name]='chain-80001'
-  [chainId]="80001"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=10000
-  [autoScalingThreshholdCPU]=2500m
-  [minReplica]=2
-  [maxReplica]=2
-)
+# declare -A chain_mumbai=(
+#   [name]='chain-80001'
+#   [chainId]="80001"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=10000
+#   [autoScalingThreshholdCPU]=2500m
+#   [minReplica]=2
+#   [maxReplica]=2
+# )
