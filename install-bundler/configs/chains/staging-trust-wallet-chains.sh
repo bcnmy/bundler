62dc9af1d2ee00879c99a4109cda8256845a564b
Takwa <takwa.gnenna@outlook.com>
2024-05-22 12:34:49 +0100

    don't deploy to staging

--- install-bundler/configs/chains/staging-trust-wallet-chains.sh
+++ install-bundler/configs/chains/staging-trust-wallet-chains.sh
@@ -1,8 +1,8 @@
-declare -A chain_optimism=(
-  [name]='chain-10'
-  [chainId]="10"
-  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
-  [autoScalingThreshholdCPU]=800m
-  [minReplica]=1
-  [maxReplica]=2
-)
+# declare -A chain_optimism=(
+#   [name]='chain-10'
+#   [chainId]="10"
+#   [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+#   [autoScalingThreshholdCPU]=800m
+#   [minReplica]=1
+#   [maxReplica]=2
+# )
