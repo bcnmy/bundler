1f580d4f37196326df341fe63ace630022d371fb
Takwa <takwa.gnenna@outlook.com>
2024-05-22 11:44:46 +0100

    deploy op instead of mumbai

--- install-bundler/configs/chains/staging-trust-wallet-chains.sh
+++ install-bundler/configs/chains/staging-trust-wallet-chains.sh
@@ -1,9 +1,8 @@
-
-# declare -A chain_mumbai=(
-#   [name]='chain-80001'
-#   [chainId]="80001"
-#   [autoScalingThreshholdHTTPRequestsPerMinute]=10000
-#   [autoScalingThreshholdCPU]=2500m
-#   [minReplica]=2
-#   [maxReplica]=2
-# )
+declare -A chain_optimism=(
+  [name]='chain-10'
+  [chainId]="10"
+  [autoScalingThreshholdHTTPRequestsPerMinute]=1000
+  [autoScalingThreshholdCPU]=800m
+  [minReplica]=3
+  [maxReplica]=3
+)
