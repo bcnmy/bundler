f77d9963b5cc299bf003e42c6229d6179848f3c4
Veljko Vranić <byblos94@gmail.com>
2024-08-09 23:14:18 +0200

    feature: add packed user op (WIP)

--- src/common/entrypoint-v7/PackedUserOperation.ts
+++ src/common/entrypoint-v7/PackedUserOperation.ts
@@ -1,64 +1,143 @@
-import { UserOperationType } from "../types";
+import { type Hex, concat, pad, slice, toHex, keccak256, encodeAbiParameters} from "viem";
+import type { UserOperationType } from "../types";
 
 // TODO: Move this to config after we refactor the config
 export const COMMON_ENTRYPOINT_V7_ADDRESSES = [
   "0x0000000071727De22E5E9d8BAf0edAc6f37da032",
 ];
 
-// TODO: Check if we are using the correct types for this struct
-// Also add a link to documentation or reference implementation
+// Definitions can be found in the ERC doc: https://eips.ethereum.org/EIPS/eip-4337#entrypoint-definition
 export interface PackedUserOperation {
-  // address sender;
-  sender: string;
-  // uint256 nonce;
+  sender: `0x${string}`;
   nonce: bigint;
-  // bytes initCode;
-  initCode: string;
-  // bytes callData;
-  callData: string;
-  // bytes32 accountGasLimits;
-  accountGasLimits: string;
-  // uint256 preVerificationGas;
+  initCode: `0x${string}`;
+  callData: `0x${string}`;
+  accountGasLimits: `0x${string}`;
   preVerificationGas: bigint;
-  // bytes32 gasFees;
-  gasFees: string;
-  // bytes paymasterAndData;
-  paymasterAndData: string;
-  // bytes signature;
-  signature: string;
+  gasFees: `0x${string}`;
+  paymasterAndData: `0x${string}`;
+  signature: `0x${string}`;
+}
+
+export function packPaymasterData(
+  paymaster: `0x${string}`,
+  paymasterVerificationGasLimit: number | bigint,
+  postOpGasLimit: number | bigint,
+  paymasterData: `0x${string}`
+): Hex {
+  return concat([
+      paymaster,
+      pad(toHex(paymasterVerificationGasLimit), { size: 16 }),
+      pad(toHex(postOpGasLimit), { size: 16 }),
+      paymasterData
+  ]) as Hex;
+}
+export function getAccountGasLimits(userOperation: UserOperationType): Hex {
+    return concat([
+        pad(toHex(userOperation.verificationGasLimit), { size: 16 }),
+        pad(toHex(userOperation.callGasLimit), { size: 16 }),
+    ]) as Hex;
+}
+
+export function unpackAccountGasLimits(accountGasLimits: Hex) {
+    return {
+        verificationGasLimit: BigInt(slice(accountGasLimits, 0, 16)),
+        callGasLimit: BigInt(slice(accountGasLimits, 16)),
+    };
+}
+
+export function getGasFees(userOperation: UserOperationType): Hex {
+    return concat([
+        pad(toHex(userOperation.maxPriorityFeePerGas), { size: 16 }),
+        pad(toHex(userOperation.maxFeePerGas), { size: 16 }),
+    ]) as Hex;
+}
+
+export function unpackGasFees(gasFees: Hex) {
+    return {
+        maxPriorityFeePerGas: BigInt(slice(gasFees, 0, 16)),
+        maxFeePerGas: BigInt(slice(gasFees, 16)),
+    };
 }
 
 export function packUserOperation(
-  userOperation: UserOperationType,
+    userOperation: UserOperationType,
 ): PackedUserOperation {
-  // TODO: replace the following stub with the actual packing code
-  return {
-    sender: userOperation.sender,
-    accountGasLimits: "0x", // TODO: How to pack this?
-    callData: userOperation.callData,
-    gasFees: "0x", // TODO: How to pack this?
-    initCode: userOperation.initCode,
-    nonce: userOperation.nonce,
-    paymasterAndData: userOperation.paymasterAndData,
-    preVerificationGas: userOperation.preVerificationGas,
-    signature: userOperation.signature,
-  };
+    return {
+        sender: userOperation.sender,
+        nonce: userOperation.nonce,
+        initCode: userOperation.initCode,
+        callData: userOperation.callData,
+        accountGasLimits: getAccountGasLimits(userOperation),
+        preVerificationGas: userOperation.preVerificationGas,
+        gasFees: getGasFees(userOperation),
+        paymasterAndData: userOperation.paymasterAndData,
+        signature: userOperation.signature,
+    };
 }
 
-// TODO: Implement this, should be the reverse of packUserOperation
 export function unpackUserOperation(
-  packedUserOperation: PackedUserOperation,
+    packedUserOperation: PackedUserOperation,
 ): UserOperationType {
-  return packedUserOperation as any as UserOperationType;
+    const { verificationGasLimit, callGasLimit } = unpackAccountGasLimits(packedUserOperation.accountGasLimits);
+    const { maxPriorityFeePerGas, maxFeePerGas } = unpackGasFees(packedUserOperation.gasFees);
+
+    return {
+        sender: packedUserOperation.sender,
+        nonce: packedUserOperation.nonce,
+        initCode: packedUserOperation.initCode,
+        callData: packedUserOperation.callData,
+        callGasLimit,
+        verificationGasLimit,
+        preVerificationGas: packedUserOperation.preVerificationGas,
+        maxFeePerGas,
+        maxPriorityFeePerGas,
+        paymasterAndData: packedUserOperation.paymasterAndData,
+        signature: packedUserOperation.signature,
+    };
 }
 
-// TODO: Rewrite the following Solidity function to TypeScript
-/// @inheritdoc IEntryPoint
-//  function getUserOpHash(
-//    PackedUserOperation calldata userOp
-//  ) public view returns (bytes32) {
-//     return keccak256(abi.encode(userOp.hash(), address(this), block.chainid));
-// }
-export function getUserOpHash(userOp: PackedUserOperation): string {
-  return userOp.toString();
+export function getUserOpHash(
+    userOp: PackedUserOperation,
+    entryPoint: `0x${string}`,
+    chainId: bigint
+): Hex {
+    const userOpEncoded = encodeAbiParameters(
+      [
+        { type: 'address' },
+        { type: 'uint256' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'uint256' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+      ],
+      [
+        userOp.sender,
+        userOp.nonce,
+        keccak256(userOp.initCode),
+        keccak256(userOp.callData),
+        userOp.accountGasLimits,
+        userOp.preVerificationGas,
+        userOp.gasFees,
+        keccak256(userOp.paymasterAndData),
+      ]
+    )
+    const userOpHash = keccak256(userOpEncoded);
+  
+    const fullEncodedData = encodeAbiParameters(
+      [
+        { type: 'bytes32' },
+        { type: 'address' },
+        { type: 'uint256' },
+      ],
+      [
+        userOpHash,
+        entryPoint,
+        chainId,
+      ]
+    );
+  
+    return keccak256(fullEncodedData);
 }
---
+++ src/common/entrypoint-v7/test/PackedUserOperation.test.ts
@@ -0,0 +1,60 @@
+import { UserOperationType } from "../../types";
+import { getUserOpHash, packUserOperation } from "../PackedUserOperation";
+import { logger } from "../../logger";
+import { assert } from "joi";
+
+const log = logger.child({
+   module: module.filename.split("/").slice(-4).join("/"),
+});
+
+describe("PackedUserOperation test", () => {
+    const ENTRYPOINT_V7_ADDRESS = "0x0000000071727De22E5E9d8BAf0edAc6f37da032";
+    const chainId = 84532n;
+
+    describe.only("user op convertion 1", () => {
+        it.only("user op with no simple fields", async () => {
+        
+        const correctHash = "0x42bfcf0ecae5b274838cba8bbe6c6012f2259014607d22248b3414337c26f25d";
+
+        const userOp: UserOperationType = {
+            sender: "0x1234567890abcdef1234567890abcdef12345678",
+            nonce: 1n,
+            initCode: "0x",
+            callData: "0x",
+            paymasterAndData: "0x",
+            callGasLimit: 98000n,
+            verificationGasLimit: 500000n,
+            preVerificationGas: 21000n,
+            maxPriorityFeePerGas: 1000000n,
+            maxFeePerGas: 13156190n,
+            signature: "0x",
+        };
+
+        const packedUserOp = packUserOperation(userOp); 
+        expect(getUserOpHash(packedUserOp, ENTRYPOINT_V7_ADDRESS, chainId)).toEqual(correctHash);
+        });
+        it.only("fail user op with no simple fields", async () => {
+        
+            const correctHash = "0x42bfcf0ecae5b274838cba8bbe6c6012f2259014607d22248b3414337c26f25d";
+    
+            const userOp: UserOperationType = {
+                sender: "0x1234567890abcdef1234567890abcdef12345678",
+                nonce: 1n,
+                initCode: "0x",
+                callData: "0x",
+                paymasterAndData: "0x",
+                callGasLimit: 98000n,
+                verificationGasLimit: 0n,
+                preVerificationGas: 21000n,
+                maxPriorityFeePerGas: 1000000n,
+                maxFeePerGas: 13156190n,
+                signature: "0x",
+            };
+    
+            
+            const packedUserOp = packUserOperation(userOp);
+            console.log(getUserOpHash(packedUserOp, ENTRYPOINT_V7_ADDRESS, chainId));
+            expect(getUserOpHash(packedUserOp, ENTRYPOINT_V7_ADDRESS, chainId) == correctHash).toEqual(false);
+            });
+    });
+});
