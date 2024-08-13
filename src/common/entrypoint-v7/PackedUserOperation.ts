bf81d461b2300299daf05d7e4c0453f4a8c4aa8a
Veljko Vranić <byblos94@gmail.com>
2024-08-13 14:53:46 +0200

    add comments

--- src/common/entrypoint-v7/PackedUserOperation.ts
+++ src/common/entrypoint-v7/PackedUserOperation.ts
@@ -19,7 +19,14 @@ export interface PackedUserOperation {
   signature: `0x${string}`;
 }
 
-
+/**
+ * Packs two unsigned 16 byte (128-bit) integers into a single hexadecimal string.
+ * The two integers are converted to hex and padded to ensure a fixed size.
+ * 
+ * @param a - The first unsigned integer (128-bit).
+ * @param b - The second unsigned integer (128-bit).
+ * @returns A Hex string that represents the packed integers.
+ */
 export function packUint(a: bigint, b: bigint): Hex {
   return concat([
     pad(toHex(a), { size: 16 }),
@@ -27,6 +34,14 @@ export function packUint(a: bigint, b: bigint): Hex {
   ]) as Hex;
 }
 
+/**
+ * Unpacks a hexadecimal string into two unsigned 16 bytes (128-bit) integers.
+ * The hex string is sliced into two parts corresponding to the original integers.
+ * 
+ * It is used to undo the application of packUint function.
+ * @param packedUints - A Hex string containing the packed integers.
+ * @returns An object containing the two unpacked integers as `a` and `b`.
+ */
 export function unpackUint(packedUints: Hex) {
     return {
       a: BigInt(slice(packedUints, 0, 16)),
@@ -58,6 +73,16 @@ export function unpackGasFees(gasFees: Hex) {
     return {maxPriorityFeePerGas, maxFeePerGas};
 }
 
+/**
+ * Packs a UserOperation object into a PackedUserOperation format.
+ * 
+ * UserOperation is a well known object in Account Abstraction.
+ * PackedUserOperation is an object introduced in EntryPoint v0.7,
+ * as an object that is being sent to EntryPoint smart contract.
+ *  
+ * @param userOperation - The UserOperationType object to be packed.
+ * @returns The PackedUserOperation object.
+ */
 export function packUserOperation(
     userOperation: UserOperationType,
 ): PackedUserOperation {
@@ -74,6 +99,12 @@ export function packUserOperation(
     };
 }
 
+/**
+ * Unpacks a PackedUserOperation object back into a UserOperationType format.
+ * 
+ * @param packedUserOperation - The PackedUserOperation object to be unpacked.
+ * @returns The UserOperationType object.
+ */
 export function unpackUserOperation(
     packedUserOperation: PackedUserOperation,
 ): UserOperationType {
@@ -122,32 +153,31 @@ export function encodePackedUserOp(
           keccak256(userOp.paymasterAndData),
         ]
       );
-    } else {
-      return encodeAbiParameters(
-        [
-          { type: 'address' },
-          { type: 'uint256' },
-          { type: 'bytes32' },
-          { type: 'bytes32' },
-          { type: 'bytes32' },
-          { type: 'uint256' },
-          { type: 'bytes32' },
-          { type: 'bytes32' },
-          { type: 'bytes'   }, 
-        ],
-        [
-          userOp.sender,
-          userOp.nonce,
-          userOp.initCode,
-          userOp.callData,
-          userOp.accountGasLimits,
-          userOp.preVerificationGas,
-          userOp.gasFees,
-          userOp.paymasterAndData,
-          userOp.signature
-        ]
-      );
-    }
+    } 
+    return encodeAbiParameters(
+      [
+        { type: 'address' },
+        { type: 'uint256' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'uint256' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes'   }, 
+      ],
+      [
+        userOp.sender,
+        userOp.nonce,
+        userOp.initCode,
+        userOp.callData,
+        userOp.accountGasLimits,
+        userOp.preVerificationGas,
+        userOp.gasFees,
+        userOp.paymasterAndData,
+        userOp.signature
+      ]
+    );
 }
 
 export function getUserOpHash(
