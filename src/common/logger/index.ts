cbeadc182a9575271ba17dd0cb0c34c7f7a4e318
Veljko Vranić <byblos94@gmail.com>
2024-10-17 13:20:01 +0200

    json logging instead of pretty priny

--- src/common/logger/index.ts
+++ src/common/logger/index.ts
@@ -3,17 +3,6 @@ import pino from "pino";
 const destination = pino.destination({ sync: false });
 
 // Create the logger using the asynchronous destination
-const logger = pino(
-  {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        destination: 1
-      },
-    },
-  },
-  destination
-);
+const logger = pino({}, destination);
 
 export { logger };
